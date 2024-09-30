5d37439aa98e569eba8cb5bf94293969700e7b75
jquie <jacquelynphan03@gmail.com>
2024-09-30 10:21:04 -0700

    Not finished :(

--- Classwork.java
+++ Classwork.java
@@ -1,10 +1,33 @@
+//FirstLastN
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    String word = FirstlastN("dictionary", 3);
+    System.out.println(word);
   }
 
-  // write solutions to problems below
+  public static String FirstlastN(String s, int n)
+  {
+    int startingIndex = s.length() - n;
+    String output = s.substring(0, startingIndex);
+  
+    int endingIndex = s.length() - n;
+    String output2 = s.substring(endingIndex, s.length());
+    return(output + output2);
+  }
+
+//DelEnd
+  {
+    String word = delEnd("computer", 5);
+    System.out.println(word);
+  }
+
+  public static String delEnd(String s, int n)
+  {
+    int stoppingIndex = s.length() - n;
+    String output = s.substring(0, stoppingIndex);
+    return output;
+  }
 
 }
