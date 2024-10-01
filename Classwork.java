33794790111807b3bc566651018540cb7cb36d84
jquie <jacquelynphan03@gmail.com>
2024-10-01 10:18:27 -0700

    not done

--- Classwork.java
+++ Classwork.java
@@ -3,10 +3,14 @@ public class Classwork
 {
   public static void main(String[] args)
   {
+    //FirstLastN
     String word = FirstlastN("dictionary", 3);
     System.out.println(word);
+    //DelEnd
     word = delEnd("computer", 5);
     System.out.println(word);
+    //CompareLowerCase
+    System.out.println(str1.comparetostr2);
   }
 
   public static String FirstlastN(String s, int n)
@@ -17,7 +21,7 @@ public class Classwork
     String output2 = s.substring(endingIndex);
     return (output + output2);
   }
-
+// DelEnd
   public static String delEnd(String s, int n)
   {
     int stoppingIndex = s.length() - n;
@@ -28,6 +32,7 @@ public class Classwork
 // ComparingLowerCase 
   public static String compareLower(String s, String i)
   {
-    
+    String str1 = s;
+    String str2 = i;
   }
 }
